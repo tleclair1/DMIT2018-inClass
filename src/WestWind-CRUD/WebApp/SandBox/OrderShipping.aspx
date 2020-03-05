53b647c304b558c0082ff3c25a1f62a346c42855
tleclair1 <54912546+tleclair1@users.noreply.github.com>
2020-03-05 16:39:31 -0700

    .

---
+++ src/WestWind-CRUD/WebApp/SandBox/OrderShipping.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="Order Processing" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="OrderShipping.aspx.cs" Inherits="WebApp.SandBox.OrderShipping" %>
+
+<%@ Register Src="~/UserControls/MessageUserControl.ascx" TagPrefix="uc1" TagName="MessageUserControl" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="page-header">Order Processing</h1>
+            <asp:Label Id="SupplierName" Text="" runat="server" />
+            <asp:Label Id="ContactName" Text="" runat="server" />
+            <uc1:MessageUserControl runat="server" ID="MessageUserControl" />
+
+            <asp:ListView ID="ShipmentListView" runat="server">
+                <ItemTemplate></ItemTemplate>
+            </asp:ListView>
+
+            <asp:HiddenField Id="TempSupplier" runat="server" Value="3"/> 
+            <asp:ObjectDataSource id="OutstandingOrderDataSource" runat="server" />
+        </div>
+
+    </div>
+</asp:Content>
--- src/WestWind-CRUD/WestWindSystem/BLL/OrderProcessingController.cs
+++ src/WestWind-CRUD/WestWindSystem/BLL/OrderProcessingController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,15 +8,18 @@ using WestWindSystem.DataModels.OrderProcessing;
 
 namespace WestWindSystem.BLL
 {
+    [DataObject]
     public class OrderProcessingController
     {
         #region Query Methods
+        [DataObjectMethod(DataObjectMethodType.Select)]
         public List<OutstandingOrder> LoadOrders(int supplierID)
         {
             // TODO: Implement LoadOrders
             throw new NotImplementedException();
         }
 
+        [DataObjectMethod(DataObjectMethodType.Select)]
         public List<ShipperSelection> ListShippers()
         {
             // TODO: Implement ListShippers
@@ -32,7 +36,15 @@ namespace WestWindSystem.BLL
         #region Command Methods
         public void ShipOrder(int orderID, ShippingDirections directions, List<ProductShipment> items)
         {
-            // TODO: Implement ShipOrder
+            // TODO: Implement ShipOrder - Validation of input:
+            //      OrderID must exist
+            //      Shipper must exist
+            //      Must have one or more items to ship
+            //      ProductIDs must exist and be valid
+            //      Quantities must be greater than 0 and less than the number / qty outstanding on the order
+            //      Freight cahrge is either null or a value greater than 0
+            // TODO: Add a new Shipment to the database
+            // TODO: Add new ManifestItem objects to the new shipment
             throw new NotImplementedException();
         }
         #endregion
